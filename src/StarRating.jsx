ab2b6a792596604ab7792f41c6a1829685f27ce3
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-11-15 13:01:33 +0530

    Add hover functionality for the rating system

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -17,6 +17,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   return (
     <div style={containerStyle}>
@@ -24,12 +25,14 @@ export default function StarRating({ maxRating = 5 }) {
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
+            isFull={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onStarSelection={() => setRating(i + 1)}
-            isFull={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ''}</p>
+      <p style={textStyle}>{tempRating || rating || ''}</p>
     </div>
   );
 }
@@ -41,9 +44,15 @@ const starStyle = {
   cursor: 'pointer',
 };
 
-function Star({ onStarSelection, isFull }) {
+function Star({ onStarSelection, isFull, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onStarSelection}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onStarSelection}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {isFull ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
