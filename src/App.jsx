37a1e9163f7691fbc96c1096bc8837e56dee53d5
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-12-01 02:59:08 +0530

    Close movie details upon addition to watched list

--- src/App.jsx
+++ src/App.jsx
@@ -230,6 +230,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
       runtime: Number(runtime.split(' ').at(0)),
     };
     onAddWatched(newWatchedMovie);
+    onCloseMovie();
   }
 
   useEffect(
